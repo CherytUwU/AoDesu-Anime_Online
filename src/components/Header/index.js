e6cee0a3ad087d3221c2c8577901579b94195af0
CherytUwU <79613492+CherytUwU@users.noreply.github.com>
2021-06-18 14:30:44 -0500

    ClassName

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,16 +17,16 @@ const Header = () => {
             <h2 className="links-menu"><Link to="/anime"><span className="menu-text">Anime</span> <span className="icon-text"><i class="fas fa-compass"></i></span></Link></h2>
             <h2 className="emision"><Link to="/en_emision"><span className="menu-text">En Emisión</span> <span className="icon-text"><i className="fas fa-clock"></i></span></Link></h2>
     
-            <div class="btnContainer">
-              <button class="search-btn" onClick="mostrarSearchInput()" id="btnSearch1"><i class="fas fa-search" aria-hidden="true"></i></button>
-              <button class="search-btn-2" onClick="ocultarSearchInput()" id="btnSearch2"><i class="fas fa-search" aria-hidden="true"></i></button>
-              <div class="container-searchInput" id="containerSearch">
-                <div class="container-color-input" id="input-container-search">
+            <div className="btnContainer">
+              <button className="search-btn" onClick="mostrarSearchInput()" id="btnSearch1"><i className="fas fa-search" aria-hidden="true"></i></button>
+              <button className="search-btn-2" onClick="ocultarSearchInput()" id="btnSearch2"><i className="fas fa-search" aria-hidden="true"></i></button>
+              <div className="container-searchInput" id="containerSearch">
+                <div className="container-color-input" id="input-container-search">
                   <form>
-                <input type="search" placeholder="Busca Tu Anime..." class="inputSearch" id="inputSearch"></input>
-                  <button class="buttonSearch">Buscar</button>
+                <input type="search" placeholder="Busca Tu Anime..." className="inputSearch" id="inputSearch"></input>
+                  <button className="buttonSearch">Buscar</button>
                   </form>
-                <div class="Q-question">
+                <div className="Q-question">
                   <a href="">¿Problemas con tu busqueda?</a>
                 </div>
                 </div>
