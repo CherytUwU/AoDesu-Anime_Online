b4c1c898451867451674f8c2679f32361fd8fe93
CherytUwU <79613492+CherytUwU@users.noreply.github.com>
2021-06-18 16:51:38 -0500

    Cambios uwu

--- src/containers/public/Home/index.js
+++ src/containers/public/Home/index.js
@@ -11,13 +11,14 @@ const Home = () => {
         </div>
         <div className="contenido">
         <Link to="/anime/boku_no_hero_academia_season_one" className="accion">
+            <div class="background"></div>
             <div className="contenedor-contenido" id="BokunoHeroAcademiaSeasonOne">
               <div className="hover-interface">
                 <div className="arrow-interface">
                   <i className="far fa-arrow-alt-circle-right"></i>
                 </div>
               </div>
-              <img loading="lazy" src="https://i.ibb.co/QXjvz4k/boku-no-hero-season1-normal.jpg" title="Boku no Hero Academia" alt="Boku no Hero Academia" />
+              <img loading="lazy" src="https://i.ibb.co/qWZshPk/boku-no-hero-season1-larga.jpg" class="imgPrincipal" title="Boku no Hero Academia" alt="Boku no Hero Academia" />
               <div className="title-contenido">
                 <p>Boku no Hero Academia</p>
               </div>
@@ -25,13 +26,14 @@ const Home = () => {
           </Link>
 
           <Link to="/anime/kimetsu_no_yaiba" className="accion">
+            <div class="background"></div>
             <div className="contenedor-contenido">
               <div className="hover-interface">
                 <div className="arrow-interface">
                   <i className="far fa-arrow-alt-circle-right"></i>
                 </div>
               </div>
-              <img loading="lazy" src="https://i.ibb.co/vVW29T6/Kimetsu-no-yaiba-season1-normal.jpg" title="Kimetsu no Yaiba" alt="Kimetsu no Yaiba" />
+              <img loading="lazy" src="https://i.ibb.co/XXqHQH2/Kimetsu-no-yaiba-season1-largo.jpg" class="imgPrincipal" title="Kimetsu no Yaiba" alt="Kimetsu no Yaiba" />
               <div className="title-contenido">
                 <p>Kimetsu no Yaiba</p>
               </div>
@@ -53,13 +55,14 @@ const Home = () => {
           </Link>
 
           <Link to="/anime/seishun_buta_yarou_wa_bunny_girl_senpai_no_yume_wo_minai" className="recuentosDeLaVida">
+            <div class="background"></div>
             <div className="contenedor-contenido">
               <div className="hover-interface">
                 <div className="arrow-interface">
                   <i className="far fa-arrow-alt-circle-right"></i>
                 </div>
               </div>
-              <img loading="lazy" src="https://i.ibb.co/r5VW72X/seishun-buta-yarou-wa-bunny-girl-senpai-no-yume-wo-minai-normal.jpg" title="Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai" alt="Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai" />
+              <img loading="lazy" src="https://i.ibb.co/gz1tt9V/seishun-buta-yarou-wa-bunny-girl-senpai-no-yume-wo-minai-largo.jpg"  class="imgPrincipal" title="Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai" alt="Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai" />
               <div className="title-contenido">
                 <p>Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai</p>
               </div>
@@ -67,13 +70,14 @@ const Home = () => {
           </Link>
 
           <Link to="/anime/jibaku_shounen_hanako-kun" className="comedia">
+            <div class="background"></div>
             <div className="contenedor-contenido">
               <div className="hover-interface">
                 <div className="arrow-interface">
                   <i className="far fa-arrow-alt-circle-right"></i>
                 </div>
               </div>
-              <img loading="lazy" src="https://i.ibb.co/x7gxY5p/Jibaku-shonen-hanako-kun-normal.jpg" title="jibaku shōnen hanako-kun" alt="jibaku shōnen hanako-kun" />
+              <img loading="lazy" src="https://i.ibb.co/12gpsNH/Jibaku-shonen-hanako-kun-largo.jpg" class="imgPrincipal" title="Jibaku Shounen Hanako-kun" alt="Jibaku Shounen Hanako-kun" />
               <div className="title-contenido">
                 <p>jibaku shōnen hanako-kun</p>
               </div>
@@ -81,14 +85,15 @@ const Home = () => {
           </Link>
 
           <Link to="/anime/kono_subarashii_sekai_ni_shukufuku_wo!_2" className="comedia">
+            <div class="background"></div>
             <div className="contenedor-contenido">
               <div className="hover-interface">
                 <div className="arrow-interface">
                   <i className="far fa-arrow-alt-circle-right"></i>
                 </div>
               </div>
-              <img loading="lazy" src="https://i.ibb.co/NS2gJhs/Kono-Subarashii-Sekai-ni-Shukufuku-wo-season2-normal.jpg" title="Kono Subarashii Sekai ni Shukufuku wo! 2" alt="Kono Subarashii Sekai ni Shukufuku wo! 2" />
-              <div className="title-contenido">
+              <img loading="lazy" src="https://i.ibb.co/Qdz12f9/Kono-Subarashii-Sekai-ni-Shukufuku-wo-season2-largo.jpg" class="imgPrincipal" title="Kono Subarashii Sekai ni Shukufuku wo!2" alt="Kono Subarashii Sekai ni Shukufuku wo!2" />
+ <div class="title-contenido2">
                 <p>Kono Subarashii Sekai ni Shukufuku wo! 2</p>
               </div>
             </div>
@@ -102,13 +107,14 @@ const Home = () => {
         </div>
         <div className="contenido">
           <Link to="anime/ijiranaide_nagatoro-san" className="recuentosDeLaVida">
+            <div class="background"></div>
             <div className="contenedor-contenido">
               <div className="hover-interface">
                 <div className="arrow-interface">
                   <i className="far fa-arrow-alt-circle-right"></i>
                 </div>
               </div>
-              <img loading="lazy" src="https://i.ibb.co/VtnXPTW/Ijiranaide-nagatoro-san-normal.jpg" title="Ijiranaide, Nagatoro-san" alt="Ijiranaide, Nagatoro-san" />
+              <img loading="lazy" src="https://i.ibb.co/Cn4tT5Z/Ijiranaide-nagatoro-san-largo.jpg" class="imgPrincipal" title="Ijiranaide, Nagatoro-san" alt="Ijiranaide, Nagatoro-san" />
               <div className="title-contenido">
                 <p>Ijiranaide, Nagatoro-san</p>
               </div>
