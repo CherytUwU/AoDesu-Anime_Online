efc7b255ff95e7510b3f64337dc8a3bbf76ddd97
CherytUwU <79613492+CherytUwU@users.noreply.github.com>
2021-06-18 21:27:31 -0500

    function

--- src/index.js
+++ src/index.js
@@ -12,6 +12,26 @@ for (var i=0;i<elems.length;i+=1){
 }
 }
 
+function mostrarSearchInput(){
+  document.getElementById('inputSearch').style.display = "block";
+  document.getElementById('btnSearch1').style.display = "none";
+  document.getElementById('btnSearch2').style.display = "block";
+  document.getElementById('containerSearch').style.display = "block";
+  document.getElementById('ocultar-search').style.display = "block";
+  document.getElementById('input-container-search').style.display = "block";
+}
+
+function ocultarSearchInput(){
+  document.getElementById('inputSearch').style.display = "none";
+  document.getElementById('btnSearch1').style.display = "block";
+  document.getElementById('btnSearch2').style.display = "none";
+  document.getElementById('containerSearch').style.display = "none";
+  document.getElementById('containerSearch').style.display = "block";
+  document.getElementById('ocultar-search').style.display = "none";
+  document.getElementById('input-container-search').style.display = "none";
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
